e81c8e8410a3c5629d127d8ee7dd347982c64a5c
FarhanDS-92 <108830582+FarhanDS-92@users.noreply.github.com>
2023-11-20 15:12:12 -0500

    final js updates

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 import { alienPlanets } from "./alienPlanets.js";
+
+let cardContainer = document.querySelector("#card-container");
+
+for (let i = 0; i < alienPlanets.length; i++) {
+  let card = document.createElement("div");
+  card.className = "card";
+  cardContainer.appendChild(card);
+
+  let name = document.createElement("h4");
+  name.textContent = `${alienPlanets[i].name}`;
+  card.appendChild(name);
+
+  let emoji = document.createElement("p");
+  emoji.textContent = `${alienPlanets[i].emoji}`;
+  card.appendChild(emoji);
+}
