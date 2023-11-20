a03a4019c7839bcd58238da6ddd6811a3587b071
FarhanDS-92 <108830582+FarhanDS-92@users.noreply.github.com>
2023-11-20 16:06:32 -0500

    updated functionality

--- index.js
+++ index.js
@@ -14,4 +14,30 @@ for (let i = 0; i < alienPlanets.length; i++) {
   let emoji = document.createElement("p");
   emoji.textContent = `${alienPlanets[i].emoji}`;
   card.appendChild(emoji);
+
+  let distance = document.createElement("p");
+  distance.textContent = `Distance from earth: ${alienPlanets[i].distanceFromEarth}`;
+
+  let atmosphere = document.createElement("p");
+  atmosphere.textContent = `Atmospher: ${alienPlanets[i].atmosphere}`;
+
+  let life = document.createElement("p");
+  life.textContent = `Does it have life: ${alienPlanets[i].hasLife}`;
+
+  card.addEventListener("mouseover", function () {
+    card.style.backgroundColor = "gray";
+
+    card.replaceChildren();
+    card.appendChild(distance);
+    card.appendChild(atmosphere);
+    card.appendChild(life);
+  });
+
+  card.addEventListener("mouseleave", function () {
+    card.replaceChildren();
+
+    card.style.backgroundColor = "white";
+    card.appendChild(name);
+    card.appendChild(emoji);
+  });
 }
